62732f427469835d69b1ad07da7f929e99b887c5
Ranjan <parmarranjan748@gmail.com>
2026-05-27 15:28:26 +0530

    learning pull

---
+++ Assignment73_PartB.java
@@ -0,0 +1,13 @@
+package Assignment70Plus;
+
+public class Assignment73_PartB extends Assignment73_PartC{
+	
+	static void ranjan()
+	{
+		int a = 80;
+		int b = 10;
+		int c = a-b;
+		System.out.println("sub from 2nd class ->" + c);
+
+	}
+}
